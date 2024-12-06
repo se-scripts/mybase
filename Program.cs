999f8e8a9fb6b7a492f28bedd0059474d2a5c2fd
chivehao <chivehao@ikaros.run>
2024-12-06 14:25:32 +0800

    优化

--- Program.cs
+++ Program.cs
@@ -22,7 +22,6 @@ namespace IngameScript
         * Base Graphical inventory display for LCD by Space Engineers Script.
         * 太空工程师，基地图形化显示库存脚本。
         * 
-        * @version 1.0.0
         * @see <https://github.com/se-scripts/mybase>
         * @author [Hi.James](https://space.bilibili.com/368005035)
         * @author [chivehao](https://github.com/chivehao)
